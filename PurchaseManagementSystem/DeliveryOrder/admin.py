5a8b87af787bff6854eda52697a55733a6e87043
Muhamad Hafiz <hafizghani.pigoh@gmail.com>
2019-05-19 15:13:11 +0800

    add delivery order

--- PurchaseManagementSystem/DeliveryOrder/admin.py
+++ PurchaseManagementSystem/DeliveryOrder/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from DeliveryOrder.models import DeliveryOrder,DeliveryOrderItem
+
+
+admin.site.register(DeliveryOrder)
+admin.site.register(DeliveryOrderItem)
