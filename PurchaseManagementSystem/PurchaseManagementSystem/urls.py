2e47841888d6420a3ab17ff0702b38aeaa0047c7
Muhamad Hafiz <hafizghani.pigoh@gmail.com>
2019-05-19 15:14:30 +0800

    update system

--- PurchaseManagementSystem/PurchaseManagementSystem/urls.py
+++ PurchaseManagementSystem/PurchaseManagementSystem/urls.py
@@ -11,6 +11,9 @@ import app.views
 import PurchaseOrder.views
 import PurchaseRequisition.views
 import RequestOfQuotation.views
+import Quotation.views
+import DeliveryOrder.views
+import Invoice.views
 
 # Uncomment the next lines to enable the admin:
 from django.conf.urls import include
@@ -47,6 +50,14 @@ urlpatterns = [
     url(r'^requestofquotationhistorydetails', RequestOfQuotation.views.requestofquotationhistorydetails, name='request_of_quotation_history_details'),
     url(r'^requestofquotationhistory', RequestOfQuotation.views.requestofquotationhistory, name="request_of_quotation_history"),
 
+    #quotation
+    url(r'^quotationform$', Quotation.views.quotationform, name="quotation_form"),
+    url(r'^fillingquotation', Quotation.views.fillingquotation, name="fill_quotation_form"),
+    url(r'^quotationconfirmation', Quotation.views.quotationconfirmation, name="confirm_quotation"),
+    url(r'^quotationdetails', Quotation.views.quotationdetails, name="quotation_details"),
+    url(r'^quotationhistorydetails', Quotation.views.quotationhistorydetails, name='quotation_history_details'),
+    url(r'^quotationhistory', Quotation.views.quotationhistory, name="quotation_history"),
+
     #purchase order
     url(r'^purchaseorderform$', PurchaseOrder.views.purchaseorderform, name="purchase_order_form"),
     url(r'^fillingpurchaseorder', PurchaseOrder.views.fillingpurchaseorder, name="fill_purchase_order_form"),
@@ -54,4 +65,20 @@ urlpatterns = [
     url(r'^purchaseorderdetails', PurchaseOrder.views.purchaseorderdetails, name="purchase_order_details"),
     url(r'^purchaseorderhistorydetails', PurchaseOrder.views.purchaseorderhistorydetails, name='purchase_order_history_details'),
     url(r'^purchaseorderhistory', PurchaseOrder.views.purchaseorderhistory, name="purchase_order_history"),
+
+    #delivery order
+    url(r'^deliveryorderform$', DeliveryOrder.views.deliveryorderform, name="delivery_order_form"),
+    url(r'^fillingdeliveryorder', DeliveryOrder.views.fillingdeliveryorder, name="fill_delivery_order_form"),
+    url(r'^deliveryorderconfirmation', DeliveryOrder.views.deliveryorderconfirmation, name="confirm_delivery_order"),
+    url(r'^deliveryorderdetails', DeliveryOrder.views.deliveryorderdetails, name="delivery_order_details"),
+    url(r'^deliveryorderhistorydetails', DeliveryOrder.views.deliveryorderhistorydetails, name='delivery_order_history_details'),
+    url(r'^deliveryorderhistory', DeliveryOrder.views.deliveryorderhistory, name="delivery_order_history"),
+
+    #Invoice
+    url(r'^invoiceform$', Invoice.views.invoiceform, name="invoiceform"),
+    url(r'^fillinginvoice', Invoice.views.fillinginvoice, name="fill_invoice_form"),
+    url(r'^invoiceconfirmation', Invoice.views.invoiceconfirmation, name="confirm_invoice"),
+    url(r'^invoicedetails', Invoice.views.invoicedetails, name="invoice_details"),
+    url(r'^invoicehistorydetails', Invoice.views.invoicehistorydetails, name='invoice_history_details'),
+    url(r'^invoicehistory', Invoice.views.invoicehistory, name="invoice_history"),
 ]
